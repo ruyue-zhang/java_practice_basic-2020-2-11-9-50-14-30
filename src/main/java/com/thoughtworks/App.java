324a1fcf928a9bff253c22b9bbcc86fc5cabe336
lpdft <772346730@qq.com>
2020-02-17 23:19:44 +0800

    [ruyue] feat: create Student and finish Student function in class

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -3,5 +3,7 @@ package com.thoughtworks;
 public class App {
 
   public static void main(String[] args) {
+    Student student = new Student("张三", "001", "2019.02.11");
+    System.out.println(student);
   }
 }
---
+++ src/main/java/com/thoughtworks/Student.java
@@ -0,0 +1,51 @@
+package com.thoughtworks;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private String id;
+    private String date;
+
+    public Student(String name, String id, String date) {
+        this.name = name;
+        this.id = id;
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id.equals(student.id);
+    }
+
+    @Override
+    public String toString() {
+        int year = 0;
+        try {
+            year = getYear();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return "我叫" + name +
+                ", 我的学号是" + id +
+                ", " + date +
+                "入学，学龄" + year + "年";
+    }
+
+    private int getYear() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.mm.dd");
+        Date enrolTime = sdf.parse(date);
+        long enrolTimeMs = enrolTime.getTime();
+        long currentTime = new Date().getTime();
+
+        return (int)Math.floor((currentTime - enrolTimeMs) / (1000 * 60 * 60 * 24 * 365));
+
+    }
+}
